d7aac377bef7451c7b72b9dcb8a93688a3a4ff5f
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2020-06-22 00:48:20 +0530

    Update port

--- index.js
+++ index.js
@@ -58,6 +58,6 @@ io.on('connection', (socket) => {
 
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+http.listen(5000, () => {
+    console.log('listening on *:5000');
 });
